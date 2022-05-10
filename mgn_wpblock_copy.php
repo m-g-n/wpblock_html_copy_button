adeb82847d635788322a1680bbe8b2f2710878bb
アクツユミ <akutsu@m-g-n.me>
2022-05-10 18:56:14 +0900

    パラメータが付与されているときのみボタンを表示する機能を作る #3
    - パラメータがある場合のみボタンを表示する
    - github actionの追加

--- mgn_wpblock_copy.php
+++ mgn_wpblock_copy.php
@@ -51,6 +51,7 @@ class Bootstrap {
 	public function __construct() {
 		add_action( 'plugins_loaded', [ $this, 'bootstrap' ] );
 		add_action( 'init', [ $this, 'load_textdomain' ] );
+		add_action( 'pre_get_posts', [ $this, 'check_allow_display_btn' ]); //TODO：もっと適切なhook名があったら修正する
 	}
 
 	/**
@@ -58,8 +59,6 @@ class Bootstrap {
 	 */
 	public function bootstrap() {
 		new App\Setup\AutoUpdate(); //自動更新チェック
-		new App\Setup\Assets();
-		$this->view_button();
 	}
 
 	/**
@@ -69,19 +68,47 @@ class Bootstrap {
 		new App\Setup\TextDomain();
 	}
 
-	public function view_button(){
+	/**
+	 * ボタンを表示するかチェック,
+	 */
+	public function check_allow_display_btn() {
+		//例外処理.
+		if ( !is_page() && !is_single() && is_front_page() ){
+			return;
+		}
+
+		//todo オプションページの値を取得
+		$view_type = 'param';
+
+		//オプションの設定内容によって表示するかの有無を判断.
+		if ( 'param' === $view_type ) { //パラメータ値で表示.
+			$param_name = 'mgn_wpblock_copy';
+			$param_val  = 'on'; //TODO：将来オプションページの値から取得
+			if ( isset($_GET[$param_name]) && $param_val === $_GET[$param_name] ) { //パラメータがある
+				$this->dislay_btn();
+			}
+		} elseif ( 'normal' === $view_type ) { //常時表示
+			$this->dislay_btn();
+		} else {
+			return;
+		}
+	}
+
+	/**
+	 * ボタンを表示.
+	 */
+	public function dislay_btn(){
+		new App\Setup\Assets(); //ボタン用のCSS・JSの読み込み.
 		add_action(
 			'wp_footer',
 			function(){
-				if ( is_page() && ! is_home() ) {
-					global $post;
-					$contents = $post->post_content;
-					?>
-					<script>
-						const copyContents = `<?php echo $contents; ?>`;
-					</script>
-					<?php
-				}
+				global $post;
+				$contents = $post->post_content;
+				?>
+				<script>
+					const copyContents = `<?php echo $contents; ?>`;
+				</script>
+				<?php
 			}
 		);
 	}
